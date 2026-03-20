2e3046d1c526b097454bd027d325beae58bc461e
bideepta saha <bideepta682@gmail.com>
2026-03-20 20:26:12 +0530

    updated version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "Hello! this is new version 4"
+    return "Hello! this is new version 5"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000)
